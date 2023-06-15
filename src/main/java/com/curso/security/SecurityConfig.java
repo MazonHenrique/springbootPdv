512bbf018355163782491e8a056d1333b3988615
Henrique Mazon <hmazon87@yahoo.com>
2023-06-15 11:04:53 -0300

    seguranca esta tudo errado

--- src/main/java/com/curso/security/SecurityConfig.java
+++ src/main/java/com/curso/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.curso.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,9 +18,10 @@ public class SecurityConfig {
     @Bean
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
-                .anyRequest().authenticated()
+                .anyRequest().authenticated().requestMatchers(HttpMethod.POST)
             );
         http.csrf((csrf) -> csrf.disable());
        
+
     }
 }
